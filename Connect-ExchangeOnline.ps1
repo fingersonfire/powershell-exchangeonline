233e76bdc637823838bf5e5d1b8947847ff65a71
FingersOnFire <fingersonfirecom+github@gmail.com>
2020-04-16 15:43:25 +0200

    Create Connect-ExchangeOnline.ps1
    Conect to Exchange Services and also MS Online should you need to get licences informations

---
+++ Connect-ExchangeOnline.ps1
@@ -0,0 +1,12 @@
+## Credentials
+$Credentials = Get-Credential
+
+## Connection into Office 365 Management
+Connect-MsolService -Credential $Credentials
+
+## Connection into Exchange Management
+$MsoExchangeURL = "https://outlook.office365.com/powershell-liveid/"
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $MsoExchangeURL -Credential $Credentials -Authentication Basic -AllowRedirection
+
+## Import session allowing to override current commands
+Import-PSSession $Session -DisableNameChecking
